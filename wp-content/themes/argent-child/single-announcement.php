67a3f07527cebc935c7d6e1eb7aacbb6d9cab92c
paramireze <paramireze@gmail.com>
2017-05-17 15:44:22 -0500

    Added announcement page

--- wp-content/themes/argent-child/single-announcement.php
+++ wp-content/themes/argent-child/single-announcement.php
@@ -11,7 +11,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'single' ); ?>
+			<?php get_template_part( 'content', 'page' ); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
@@ -26,5 +26,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
