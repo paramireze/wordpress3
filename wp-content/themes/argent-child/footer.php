cbdc22732715c5bab13c1a9a31ae17382b90a701
paramireze <paramireze@gmail.com>
2017-05-30 10:43:28 -0500

    Added copy right year

---
+++ wp-content/themes/argent-child/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Argent
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+            &copy; 2009 – <?php echo date('Y'); ?> Madison Hash House Harriers
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
